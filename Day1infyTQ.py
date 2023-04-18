59e8d0d947a9ac4001078ee7f20b60ae64f9bd26
Aman3.O <112274867+AmAn-2O@users.noreply.github.com>
2023-04-18 22:42:41 +0530

    Update Day1infyTQ.py

--- Day1infyTQ.py
+++ Day1infyTQ.py
@@ -1,31 +1,31 @@
-# def calculate_total_ticket_cost(no_of_adults, no_of_children):
-#     total_ticket_cost=0
-#     #Write your logic here
-#     children_cost=1/3*37550
-#     total_ticket=37550*no_of_adults+children_cost*no_of_children
-#     service=total_ticket*0.07
-#     discount=(total_ticket+service)*0.10
-#     total_ticket_cost=total_ticket+service-discount
-#     return total_ticket_cost
+def calculate_total_ticket_cost(no_of_adults, no_of_children):
+    total_ticket_cost=0
+    #Write your logic here
+    children_cost=1/3*37550
+    total_ticket=37550*no_of_adults+children_cost*no_of_children
+    service=total_ticket*0.07
+    discount=(total_ticket+service)*0.10
+    total_ticket_cost=total_ticket+service-discount
+    return total_ticket_cost
 
 
-# #Provide different values for no_of_adults, no_of_children and test your program
-# total_ticket_cost=calculate_total_ticket_cost(5,2)
-# print("Total Ticket Cost:",total_ticket_cost)
+#Provide different values for no_of_adults, no_of_children and test your program
+total_ticket_cost=calculate_total_ticket_cost(5,2)
+print("Total Ticket Cost:",total_ticket_cost)
 
-# ticket_status="Confirmed"
-# luggage_weight=32
-# weight_limit=30  #Weight limit for the airline
-# extra_luggage_charge=0
-# if(ticket_status=="Confirmed"):
-#     if(luggage_weight>0 and luggage_weight<=weight_limit):
-#         print("Check-in cleared")
-#     elif(luggage_weight<=(weight_limit+10)):
-#         extra_luggage_charge=300*(luggage_weight-weight_limit)
-#     else:
-#         extra_luggage_charge=500*(luggage_weight-weight_limit)
-#     if(extra_luggage_charge>0):
-#         print("Extra luggage charge is Rs.", extra_luggage_charge)
-#         print("Please make the payment to clear check-in")
-# else:
-#     print("Sorry, ticket is not confirmed")
+ticket_status="Confirmed"
+luggage_weight=32
+weight_limit=30  #Weight limit for the airline
+extra_luggage_charge=0
+if(ticket_status=="Confirmed"):
+    if(luggage_weight>0 and luggage_weight<=weight_limit):
+        print("Check-in cleared")
+    elif(luggage_weight<=(weight_limit+10)):
+        extra_luggage_charge=300*(luggage_weight-weight_limit)
+    else:
+        extra_luggage_charge=500*(luggage_weight-weight_limit)
+    if(extra_luggage_charge>0):
+        print("Extra luggage charge is Rs.", extra_luggage_charge)
+        print("Please make the payment to clear check-in")
+else:
+    print("Sorry, ticket is not confirmed")
